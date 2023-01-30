23adb4b6b5c1e3fde19d785ab972e42027b37d1a
Jonathan Vilchis <joco_va@hotmail.com>
2023-01-30 17:17:52 +0100

    promise-async

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import "./css/style.css"; //importing css
+
+let modal = document.querySelector(".modal")
+let span = document.querySelector(".close")
+let btn = document.querySelector(".btn")
+
+const myPromise = () => {
+    return new Promise((resolve) => {
+
+        setTimeout(() => {
+            resolve(modal.style.display = "block")
+        }, 60000);
+    });
+}
+
+const resolvingPromise = async () => {
+    let result = await myPromise();
+    return result
+}
+
+resolvingPromise()
+
+span.style.cursor = "pointer"
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+// btn promise
+
+const promesa = new Promise((resolve, reject) => {
+    resolve();
+});
+
+const btnResolving = async () => {
+    btn.addEventListener("animationend", () => {
+        // btn.style.backgroundColor = "aqua";
+        alert("Continue to subscribe")
+    })
+    btn.addEventListener("mouseover", () => {
+        btn.style.backgroundColor = "aqua";
+    })
+    btn.addEventListener("mouseout", () => {
+        btn.style.backgroundColor = "";
+    });
+}
+
+btnResolving()
